99a10d5d77c25b04ce52b58c2905c3c96556f2a4
Collins22 <nwosucollins35@gmail.com>
2024-09-19 16:06:03 +0100

    blaze

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
 import Services from './components/Services/Services';
+import Title from './components/Title/Title';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <Services />
+      <Title subTitle='Our PRACTICES' title ='What We Offer'/>
     </div>
   );
 }
---
+++ src/components/Practices/Practice.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Practice.css'
+
+const Practice = () => {
+  return (
+    <div className='offer'>
+        <div className='offers'>
+              <h3>Corprate Law</h3>
+              <p>We Provide expert guidiance on a wide range of corporate matters, including business formation, mergers, and acquisitions, corporate governance, and compliance. We help businesses navigate complex legal landscapes and achieve their strategic objectives.</p>
+              <h3>Family Law</h3>
+              <p>We also offer compassionate guidiance on family law matters, such as divorce, child custody, child support and property division. We strive to help clients navigate these challenging times with sensitivity and professionalism.</p>
+              <h3>Corprate Law</h3>
+              <p>We Provide expert guidiance on a wide range of corporate matters, including business formation, mergers, and acquisitions, corporate governance, and compliance. We help businesses navigate complex legal landscapes and achieve their strategic objectives.</p>
+              <h3>Corprate Law</h3>
+              <p>We Provide expert guidiance on a wide range of corporate matters, including business formation, mergers, and acquisitions, corporate governance, and compliance. We help businesses navigate complex legal landscapes and achieve their strategic objectives.</p>
+        </div>
+    </div>
+  );
+}
+
+export default Practice;
