3b6d904c4ac7797471e156d4693ff768b8f7519f
Collins22 <nwosucollins35@gmail.com>
2024-09-19 15:00:30 +0100

    blaze

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -12,7 +12,7 @@ const Services = () => {
                     <div className='caption'> 
                         <h3>Why Choose Our Top lawyers at Oma's Legal</h3>
                           <p className='p1'> "Our team of Professionals in this field are committed to providing and safe guarding the businesses, rights and legal opinions of our clients.</p>
-                          <p className='p2'>The members of our firm are committed to providing succesful guidiances as well as legal services to our clients in terms of their business, and of their legal rights. We bring in the best brains so as to provide trustworthy services that bring smiles to our clients.</p>
+                          <p className='p2'>The members of our firm are committed to providing succesful guidiances as well as legal services to our clients in terms of their business, and of their legal rights. We bring in the best brains so as to provide trustworthy services that bring smiles to our clients.</p> dyrfy
                     </div> 
       </div>
   );
