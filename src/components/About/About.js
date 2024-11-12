1ae398b58d36778b1351fcb3bbf628e0e08ec851
Collins22 <nwosucollins35@gmail.com>
2024-11-12 17:06:03 +0100

    blaze

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -9,10 +9,10 @@ const About = () => {
               <img src={about_img} className='about-img' />
             </div>   
           <div className='about-right'>
-              <h3 className='alpha'>ABOUT OMA'S LEGAL</h3>
+              <h3>ABOUT OMA'S LEGAL</h3>
               <h2>Let Us Protect Your Treasured Future</h2>
-              <p> </p>
-              <p> </p>
+              <p>Oma's Legal has established itself as a trusted and welcoming law firm in the heart of .... for over ... years. Founded by ... , a dedicated and experienced attorney, the firm offers compassionate and client-centerd legal support across various areas, from corporate law to family matters and criminal defence. Known for their commitment to understanding each clients unique needs, Oma's Legal tailors strategies to achieve the best possible outcomes.</p>
+              <p> Beyond their legal services. Oma's legal is actively engaged in the community, offering pro bono work and supporting charitable cases aligned with their mission of justice and compassion, when you work with Oma's Legal, your choosing a partner commited to defendinf your rights and pursuing justice with integrity.</p>
           </div>   
     </div>
   );
